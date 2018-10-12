76f1615fca7ed19a7d15c983b1a5ea66d552fddc
ni11c <nicolas.destrez@gmail.com>
2018-10-12 17:16:47 +0200

    Added valid twitter api credentials

--- TweetCollectorConsole/Program.cs
+++ TweetCollectorConsole/Program.cs
@@ -5,9 +5,14 @@ namespace TweetCollectorConsole
 {
     static class Program
     {
+        private const string ConsumerApiKey = "Al7LMCED4TL6mELVEN9VC1k3q";
+        private const string ConsumerApiSecret = "nxqYrh1dLlmzmuXD5NfN3wtfOVBBmHqWqG48K8tGu8NhuT9zT3";
+        private const string AccessToken = "50558915-WbZicbfrssfHn4IcsfHbYXzMGFoc5EmYpFvfzyzsM";
+        private const string AccessTokenSecret = "aau441Nz7Q1nP1XYQvIPtkDRZRcgUaW2nsFAUeDMBTz5P";
+
         static void Main(string[] args)
         {
-            Auth.SetUserCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "USER_ACCESS_TOKEN", "USER_ACCESS_SECRET");
+            Auth.SetUserCredentials(ConsumerApiKey, ConsumerApiSecret, AccessToken, AccessTokenSecret);
             var user = User.GetAuthenticatedUser();
             Console.WriteLine(user);
             Console.ReadKey();
