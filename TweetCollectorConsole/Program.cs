1d79d2b41be840c00e0298da65c60d92c39ffba2
Destrez Nicolas <Nicolas.Destrez@agridea.ch>
2018-10-12 15:44:28 +0200

    Initial commit

---
+++ TweetCollectorConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Tweetinvi;
+
+namespace TweetCollectorConsole
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Auth.SetUserCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "USER_ACCESS_TOKEN", "USER_ACCESS_SECRET");
+            var user = User.GetAuthenticatedUser();
+            Console.WriteLine(user);
+            Console.ReadKey();
+        }
+    }
+}
